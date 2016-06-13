749f0dc5242f0d0d97ebfc7bf4cb6c2df7465079
hidenorly <twitte.harold@gmail.com>
2016-06-14 03:48:52 +0900

    Add triming mechanism within window period and initiate it per specified cycle period

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 ```
 # gem install mqtt
 ```
+
+## How to use
+
+```
+$ ./mqttStore.rb -t /hidenorly/sensor/# -h 192.168.10.1
+```
--- mqttStore.rb
+++ mqttStore.rb
@@ -55,6 +55,25 @@ def isConnected
 	return $g_mqttClient.connected?
 end
 
+# --- utility
+def getOldIndex(data, targetTime)
+	found = false
+	i = 0
+	n = data.length
+	while ( i < n ) do
+		if data[i][:time] < targetTime then
+			i=i+1
+			found = true
+		else
+			if found then
+				return i-1
+			end
+			return -1
+		end
+	end
+	return -1
+end
+
 #---- main --------------------------
 options = {
 	:host => "localhost",
@@ -65,7 +84,9 @@ options = {
 	:client_id => nil,
 	:topics => [],
 	:outDir => ".",
-	:cacheDir => "."
+	:cacheDir => ".",
+	:updateCycle => 5,
+	:windowPeriod => 3600
 }
 
 opt_parser = OptionParser.new do |opts|
@@ -106,6 +127,14 @@ opt_parser = OptionParser.new do |opts|
 	opts.on("", "--client_id=", "Set client ID (default:#{options[:client_id]}") do |client_id|
 		options[:client_id] = client_id
 	end
+
+	opts.on("-u", "--updateCycle=", "Set updateCycle [Sec] (default:#{options[:updateCycle]}") do |updateCycle|
+		options[:updateCycle] = updateCycle.to_i
+	end
+
+	opts.on("-w", "--windowPeriod=", "Set windowPeriod [Sec] (default:#{options[:windowPeriod]}") do |windowPeriod|
+		options[:windowPeriod] = windowPeriod.to_i
+	end
 end.parse!
 
 $g_options = convOptions(options)
@@ -113,23 +142,55 @@ $g_options = convOptions(options)
 client = connectMqtt()
 client.subscribe(options[:topics])
 
+$g_updateCycle = options[:updateCycle]
+$g_windowPeriod = options[:windowPeriod]
+
 a = Thread.new {
-	aMes = {:topic=>"", :message=>""}
+	topicDataStore = {}
+	lastTime = Time.now
 	while( isConnected() ) do
+		# to minimize blocking time on main thread
+		data = []
 		$g_criticalSection.synchronize{
 			$g_eventFlag.wait($g_criticalSection)
-			aMes = $g_result.pop() if $g_result.size>0
+			while( $g_result.size>0 ) do
+				data << $g_result.pop()
+			end
 		}
-		puts "A THREAD:#{aMes[:topic]}: #{aMes[:message]}"
+		# store received data to sorted topic data
+		data.each do |aMes|
+#			puts "A THREAD:#{aMes[:time]} : #{aMes[:topic]} : #{aMes[:message]}"
+			topicDataStore[ aMes[:topic] ] = Array.new if !topicDataStore.has_key?(aMes[:topic])
+			last = topicDataStore[ aMes[:topic] ].last
+			if last==nil || last[:time]<aMes[:time] then
+				topicDataStore[ aMes[:topic] ].push( { :time=>aMes[:time], :value=>aMes[:message] } )
+			end
+		end
+		# update data
+		now = Time.now
+		if (now-lastTime)>$g_updateCycle then
+			lastTime = now
+			targetTime = now - $g_windowPeriod
+#			puts "updated #{lastTime} / #{targetTime}"
+			topicDataStore.each do |topic, data|
+				# trim data within window period
+				index = getOldIndex(data, targetTime)
+				if index!=-1 then
+					# TODO : write data to file as append
+					data.slice!(0,index)
+				end
+				puts data # output this trimed data to cache file
+			end
+		end
 	end
 }
 
 while( isConnected() ) do
 	client.get() do |topic, message|
 		$g_criticalSection.synchronize{
-			$g_result.push({:topic=>topic, :message=>message})
+			$g_result.push({:time=>Time.now, :topic=>topic, :message=>message})
 		}
-		$g_eventFlag.broadcast()
+		$g_eventFlag.signal() #broadcast()
 	end
 end
 
